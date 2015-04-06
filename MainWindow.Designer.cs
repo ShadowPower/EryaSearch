00582b2eea45617446813fc1a7f5cb9673a9b9bc
ShadowPower <617274873@qq.com>
2015-04-06 14:33:20 +0800

    Upload

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace 尔雅批量刷答案
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.searchButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.fontButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(13, 13);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(342, 23);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "复制所有题目，点击获取答案";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.outputBox.Location = new System.Drawing.Point(13, 43);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputBox.Size = new System.Drawing.Size(342, 531);
+            this.outputBox.TabIndex = 1;
+            // 
+            // fontButton
+            // 
+            this.fontButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fontButton.Location = new System.Drawing.Point(12, 580);
+            this.fontButton.Name = "fontButton";
+            this.fontButton.Size = new System.Drawing.Size(343, 23);
+            this.fontButton.TabIndex = 2;
+            this.fontButton.Text = "设置字体";
+            this.fontButton.UseVisualStyleBackColor = true;
+            this.fontButton.Click += new System.EventHandler(this.fontButton_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 615);
+            this.Controls.Add(this.fontButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.searchButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "尔雅什么的……最变态了！";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button fontButton;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Runtime.InteropServices;
+
+namespace 尔雅批量刷答案
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //重绘窗口的Windows API
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static public extern bool UpdateWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// 返回剪切板的内容
+        /// </summary>
+        /// <returns>剪切板有文本则返回文本，否则返回空字符串</returns>
+        public static string getClipboardText()
+        {
+            //获取剪切板
+            IDataObject iData = Clipboard.GetDataObject();
+            //如果内容是HTML或者TEXT，返回
+            if (iData.GetDataPresent(DataFormats.Html) || iData.GetDataPresent(DataFormats.Text))
+            {
+                return (String)iData.GetData(DataFormats.Text);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 返回从尔雅题库取得的答案
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns>字符串，多行的答案</returns>
+        private static string getAnswer(string keyword)
+        {
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create("http://erya.hang.im/search/");
+                myRequest.Method = "POST";
+                //经测试，必须指定Host否则返回HTTP 500
+                myRequest.Host = "erya.hang.im";
+                //伪装火狐
+                myRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0";
+                myRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                myRequest.Referer = "http://erya.hang.im/";
+
+
+                //参数经过URL编码
+                string paraUrlCoded = "keyword";
+                //构造请求主体
+                paraUrlCoded += "=" + System.Web.HttpUtility.UrlEncode(keyword);
+                byte[] payload;
+                //将URL编码后的字符串转化为字节
+                payload = System.Text.Encoding.UTF8.GetBytes(paraUrlCoded);
+
+                myRequest.ContentLength = payload.Length;
+
+                Stream newStream = myRequest.GetRequestStream();
+                //发送
+                newStream.Write(payload, 0, payload.Length);
+                newStream.Close();
+
+                //获得请求响应
+                HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+                StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+
+                //声明要返回的字符串
+                string returnText = "";
+
+                try
+                {
+                    XmlDocument xm = new XmlDocument();
+                    xm.LoadXml(reader.ReadToEnd());
+                    XmlNodeList listNodes = xm.SelectNodes("/ul/li/p");
+                    int lineCount = 0;
+
+                    foreach (XmlNode xmNode in listNodes)
+                    {
+                        lineCount++;
+                        //xmNode.Name
+                        if (lineCount % 2 == 0)
+                        {
+                            //如果是双数行，可能是答案行
+                            if (xmNode.InnerText == "正确")
+                            {
+                                returnText += "【√】 ■";
+                            }
+                            else if (xmNode.InnerText == "错误")
+                            {
+                                returnText += "【×】 □";
+                            }
+                            else
+                            {
+                                returnText += "    ■ ";
+                            }
+                        }
+                        returnText += xmNode.InnerText;
+                        returnText += Environment.NewLine;
+                    }
+                }
+                catch (XmlException)
+                {
+                    return "错误：从服务器返回的数据无法解析，可能是校园网未登录";
+                }
+
+                if (returnText == "")
+                {
+                    returnText = "卧槽居然没有答案" + Environment.NewLine;
+                }
+
+                return returnText;
+            }
+            catch(WebException)
+            {
+                return "网络连接中断";
+            }
+        }
+
+        /// <summary>
+        /// 核心函数，从剪切板读题目，获取答案并显示
+        /// </summary>
+        public void startSearch()
+        {
+            //读入剪切板
+            string inputText = getClipboardText();
+
+            if (inputText.Trim() != "")
+            {
+                outputBox.Text = "";
+                //匹配题目的正则表达式
+                string regexText = "\\d+、.*\\s[(]\\d{1,3}\\.\\d{1,2}分[)]";
+                //如果能匹配到题目
+                if (Regex.IsMatch(inputText, regexText))
+                {
+                    //存放所有题目的集合
+                    MatchCollection titles = Regex.Matches(inputText, regexText);
+                    //遍历数组操作每一题
+                    foreach (Match title in titles)
+                    {
+                        //提取题目序号
+                        string orderNumber = Regex.Match(title.Value, "\\d+、").Value;
+                        //提取题目核心文本
+                        string searchText = Regex.Replace(title.Value, "\\d+、", "");
+                        searchText = Regex.Replace(searchText, "[(]\\d{1,3}\\.\\d{1,2}分[)]", "");
+                        //删首尾空格
+                        searchText = searchText.Trim();
+                        outputBox.Text += "题号：" + orderNumber + "-----------------" + Environment.NewLine;
+                        outputBox.Text += getAnswer(searchText) + Environment.NewLine;
+                        
+                        //刷新自身窗口
+                        UpdateWindow(this.Handle);
+                    }
+                }
+                else
+                {
+                    //如果剪切板的内容不包含题目文本
+                    outputBox.Text = "_(:3」∠)_卧槽为什么没有题目！！" +
+                        Environment.NewLine + Environment.NewLine +
+                        "剪切板中需要格式正确的题目" +
+                        Environment.NewLine + Environment.NewLine +
+                        "请用谷歌浏览器直接复制所有题目(=・ω・=)";
+                }
+            }
+            else
+            {
+                //如果剪切板的内容是空格、空白或者非文字
+                outputBox.Text = "你的剪切板里没有文字=-=...";
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            startSearch();
+        }
+
+        private void fontButton_Click(object sender, EventArgs e)
+        {
+            //改变字体
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.Font = outputBox.Font;
+            if (fontDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                outputBox.Font = fontDialog.Font;  
+            }
+        }
+    }
+}
